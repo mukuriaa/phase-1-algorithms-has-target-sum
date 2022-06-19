298c15f56e18f7b897da6e5b16e1dc1a1f1b8e87
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-19 01:04:16 -0700

    commit tree 05393a9b5203c88b3ccb0adc8a800e89db223940

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // Iterating through the loop by comparing the index number with all the items in the array,
+  // ... to confirm if the adds up to the target. If index at [0] fails to find the total sum number, i move to index [1] and compare 
+  // with all the items in the array and so on untill its done
+
+  for (var i = 0; i< array.length; i++){
+    for (var j = i+1; j < array.length; j++){
+      if (array[i] + array[j] === target){
+        return true
+      };
+       
+    }
+  }
+  return false
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+  O(N^2)
 */
 
 /* 
   Add your pseudocode here
+  Loop through the array starting with the index[0] comaring with each item in the array list
+  if if finds an add sum Number that makes it sum to the rarget;
+  return true
+  else keep looping with the second item;
 */
 
 /*
   Add written explanation of your solution here
+  Iterating through the loop by comparing the index number with all the items in the array,
+  ... to confirm if the adds up to the target. If index at [0] fails to find the total sum number, i move to index [1] and compare 
+  with all the items in the array and so on untill its done
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +51,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
 }
 
 module.exports = hasTargetSum;
